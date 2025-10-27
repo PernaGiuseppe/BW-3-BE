1967eabe68009aff659142eacbe6df8f0be7c1fa
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-10-27 09:32:59 +0100

    Initial commit

---
+++ src/test/java/buildweek5/BW_3_BE/Bw3BeApplicationTests.java
@@ -0,0 +1,13 @@
+package buildweek5.BW_3_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw3BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
