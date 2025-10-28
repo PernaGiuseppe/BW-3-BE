87162c3feb017afe6932dd2cd00a8efc1d0c330d
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-10-28 09:33:04 +0100

    up

--- src/main/java/buildweek5/BW_3_BE/repositories/ProvinciaRepository.java
+++
@@ -1,9 +0,0 @@
-package buildweek5.BW_3_BE.repositories;
-
-import buildweek5.BW_3_BE.entities.Provincia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProvinciaRepository extends JpaRepository<Provincia, String> {
-}
